8691dab55e84494cfa378d3e225691a074e80ce6
Atul Verma <atverma@microsoft.com>
2018-12-23 07:42:20 -0500

    Added ASP.NET Core 2.2 Web App sample for IIS in process and out of process hosting

---
+++ SampleWebApp/SampleWebAppForIIS/Controllers/ValuesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SampleWebAppForIIS.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { Process.GetCurrentProcess().ProcessName };
+        }
+    }
+}
